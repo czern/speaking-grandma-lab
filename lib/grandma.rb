5fbc2253457d6674c55f5907eb8ffe65564a0e9d
Charli Zern <charlizern@me.com>
2017-06-06 20:55:46 +0000

    finished grandma lab

--- lib/grandma.rb
+++ lib/grandma.rb
@@ -1,2 +1,28 @@
-# Write your code here!
+def speak_to_grandma(words)
+    if (words == words.upcase) && (words != "BYE")
+        "NO, NOT SINCE #{1930 + rand(20)}!"
+    else
+        "HUH?! SPEAK UP, SONNY!"
+    end
+end
+
+words = "x"
+bye = 0
+
+while bye != 3
+    puts "What do you want to say to grandma?"
+    words = gets.chomp
+    if words != "BYE"
+        bye = 0
+    else
+        bye += 1
+   if bye == 3
+    puts "bye then, ):"
+        end
+     if bye != 3
+         puts speak_to_grandma(words)
+     end
+    end
+end
+
 
